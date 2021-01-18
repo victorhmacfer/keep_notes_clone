8d2b7ffa6f85f3ec11632fdc6acb7769d75ef7ec
victor <victorhmacfer@gmail.com>
2021-01-18 16:15:34 -0300

    remember last note card mode used

--- lib/notifiers/note_card_mode.dart
+++ lib/notifiers/note_card_mode.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 enum NoteCardMode { extended, small }
 
 class NoteCardModeSelection with ChangeNotifier {
-  //TODO: should be initialized to what the user used last time...
-  // for now it will always start in small mode.
-  NoteCardMode _mode = NoteCardMode.small;
+  
+  // mode is stored in SharedPreferences with key 'notecard_mode'
+  // value ==> true for small mode, false for extended mode.
+  NoteCardMode _mode; 
+
+  NoteCardModeSelection() {
+    _loadModeFromPrefs();
+  }
 
   NoteCardMode get mode => _mode;
 
   void switchTo(NoteCardMode newMode) {
     _mode = newMode;
+    _setModeInPrefs();
     notifyListeners();
+  }
+
+  _loadModeFromPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    // if value has never been set, mode is set and loaded as 'small'
+    _mode = (prefs.getBool('notecard_mode') ?? true)
+        ? NoteCardMode.small
+        : NoteCardMode.extended;
+  }
+
+  _setModeInPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    prefs.setBool(
+        'notecard_mode', (_mode == NoteCardMode.small) ? true : false);
   }
 }
